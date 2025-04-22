ce73b47b3e751acf304c69beac7736d984e401ce
chedli01 <chedli.masmoudi01@gmail.com>
2025-04-22 01:13:26 +0100

    getUsers Query

--- src/resolvers/Query.mjs
+++ src/resolvers/Query.mjs
@@ -3,6 +3,10 @@ export const Query={
     hi:async (parent,args,context,info)=> {
         const users = await context.db.collection('users').find().toArray()
       return `Hello from DB! Total users: ${users.length}` 
+    },
+    getUsers:async (parent,args,{db},info)=>{
+        return await db.collection('users').find().toArray()
+
     }
 
 }
